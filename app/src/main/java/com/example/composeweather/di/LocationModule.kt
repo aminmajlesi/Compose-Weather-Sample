934fd1fc29b56d6a763bc96303ca4efb54b912b8
aminmajlesi <majlesi@amnpardaz.ir>
2023-10-10 15:04:39 +0330

    add di

---
+++ app/src/main/java/com/example/composeweather/di/LocationModule.kt
@@ -0,0 +1,20 @@
+package com.example.composeweather.di
+
+import com.example.composeweather.data.location.DefaultLocationTracker
+import com.example.composeweather.domain.location.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
