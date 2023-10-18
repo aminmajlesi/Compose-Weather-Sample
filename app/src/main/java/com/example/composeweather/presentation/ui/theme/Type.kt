ea5256e29ebeb0a77a1c7767ac61d4401c17a078
aminmajlesi <majlesi@amnpardaz.ir>
2023-10-18 10:18:32 +0330

    add composable ui part2

--- app/src/main/java/com/example/composeweather/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/composeweather/presentation/ui/theme/Type.kt
@@ -11,24 +11,18 @@ val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 16.sp
     )
     /* Other default text styles to override
-    titleLarge = TextStyle(
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 12.sp
     )
     */
 )
